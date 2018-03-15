85b040cf8cbbf494d3c18d1b5fa892bf532e7faf
Jason Domican <jadomican@gmail.com>
2018-03-15 12:03:39 +0000

    Update py lab

--- PythonLab/python.py
+++ PythonLab/python.py
@@ -1,3 +1,4 @@
+'''
 #Iterative (task 1)
 def iterativesum():
     total = 0
@@ -11,9 +12,29 @@ def sumrange(start, end, total):
         return total
     return sumrange(start + 1, end, total + start)
 
-
 sumtotal = sum(range(1, 11))
 
 print(iterativesum())
 print(sumrange(1, 10, 0))
 print(sumtotal)
+'''
+
+'''
+#Task 2
+list = [4, 6, 9, 3, 6, 2, 6, 1, 6, 8, 2]
+print(list)
+unique_list = set(list)     #Convert list to set - collection of distinct objects
+print(unique_list)
+'''
+
+name = input("Please enter your name: ")
+print("Hello, " + name)
+
+age = int(input("Please enter your age: "))
+
+if age > 18:
+    print("You are an adult")
+else:
+    print("You are a child")
+
+list 
