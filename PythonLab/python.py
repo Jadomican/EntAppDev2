22ec2ad94ae5f8e703405d650f733680bdb0ea24
Jason Domican <jadomican@gmail.com>
2018-03-17 14:33:16 +0000

    Complete more tasks in Python lab

--- PythonLab/python.py
+++ PythonLab/python.py
@@ -27,6 +27,7 @@ unique_list = set(list)     #Convert list to set - collection of distinct object
 print(unique_list)
 '''
 
+'''
 name = input("Please enter your name: ")
 print("Hello, " + name)
 
@@ -36,5 +37,48 @@ if age > 18:
     print("You are an adult")
 else:
     print("You are a child")
+'''
 
-list 
+'''
+#Task 3
+def hyphenated(sentence):
+    words = sentence.split()
+    words.sort()
+    joined = '-'.join(words)
+    return joined
+
+sentence = input("Enter a string of words: ")
+print(hyphenated(sentence))
+'''
+
+'''
+#https://stackoverflow.com/questions/30671876/trial-division-faster-than-sieve-for-primality-test
+#Task 5
+import math
+def is_prime(number):
+    u = math.floor(math.sqrt(number))
+    i = 2
+    while (i <= u):
+        if (number % i == 0):
+            return False
+        i+= 1
+    return True
+
+print(is_prime(7))  #True
+print(is_prime(71)) #True
+print(is_prime(10)) #False
+'''
+
+#Task 6
+def is_palindrome_simple(word):
+    return word == word[::-1]
+print(is_palindrome_simple('JasaJ'))
+
+def is_palindrome_recur(word):
+    if len(word) < 2:
+        return True
+    if word[0] != word[-1]:
+        return False
+    return is_palindrome_recur(word[1:-1])
+print(is_palindrome_recur('racecar'))
+print(is_palindrome_recur('Jason'))
