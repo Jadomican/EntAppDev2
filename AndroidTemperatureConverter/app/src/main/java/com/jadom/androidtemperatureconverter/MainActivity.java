4f1ff724e1b143aefb12076df1dfde0ab051c4e7
Jason Domican <jadomican@gmail.com>
2018-04-11 12:36:00 +0100

    Android sample temperature converter update

--- AndroidTemperatureConverter/app/src/main/java/com/jadom/androidtemperatureconverter/MainActivity.java
+++ AndroidTemperatureConverter/app/src/main/java/com/jadom/androidtemperatureconverter/MainActivity.java
@@ -2,6 +2,7 @@ package com.jadom.androidtemperatureconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -9,43 +10,62 @@ import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioGroup radioGroup;
+    EditText celsiusET;
+    EditText fahrenheitET;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radio_group);
+        celsiusET = findViewById(R.id.celsiusEditText);
+        fahrenheitET = findViewById(R.id.fahrenheitEditText);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioF:
+                        fahrenheitET.setEnabled(false);
+                        celsiusET.setEnabled(true);
+                        break;
+                    case R.id.radioC:
+                        fahrenheitET.setEnabled(true);
+                        celsiusET.setEnabled(false);
+                }
+
+            }
+        });
+
     }
 
     public void convert(View v) {
 
-        EditText celsiusET = findViewById(R.id.celsiusEditText);
         String celsius = celsiusET.getText().toString().trim();
-
-        RadioGroup radioGroup = findViewById(R.id.radio_group);
-
-//        RadioButton radioC = findViewById(R.id.radioC);
-//        RadioButton radioF = findViewById(R.id.radioF);
-
-        radioGroup.getCheckedRadioButtonId();
-
-        EditText fahrenheitET = findViewById(R.id.fahrenheitEditText);
         String fahrenheit = fahrenheitET.getText().toString().trim();
 
+        if (celsius.equals("")){
+            celsius = ("0");
+        }
+        if (TextUtils.isEmpty(fahrenheit))
+        {
+            fahrenheit = "0";
+        }
+
+
+            radioGroup.getCheckedRadioButtonId();
+
+
         if (radioGroup.getCheckedRadioButtonId() == R.id.radioC) {
-            celsiusET.setText(((Double.parseDouble(fahrenheit) - 32) * (5.0 / 9.0)) + "");
+            double d = ((Double.parseDouble(fahrenheit) - 32) * (5.0 / 9.0));
+            celsiusET.setText(getString(R.string.degrees, Double.toString(d)));
 
         }
 
         if (radioGroup.getCheckedRadioButtonId() == R.id.radioF) {
-            fahrenheitET.setText(((Double.parseDouble(celsius) * (9.0 / 5.0)) + 32) + "");
-        }
-
-
-        if (celsius.length() != 0)                // convert to fahrenheit
-        {
-        } else {
-            if (fahrenheit.length() != 0)        // convert to celsius
-            {
-            }
+            double d = ((Double.parseDouble(celsius) * (9.0 / 5.0)) + 32);
+            fahrenheitET.setText(getString(R.string.degrees, Double.toString(d)));
         }
 
     }
