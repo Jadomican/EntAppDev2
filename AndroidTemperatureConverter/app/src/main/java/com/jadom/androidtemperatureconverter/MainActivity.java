1c2770d64a4a72b5eaaf39035af09b175bacc444
Jason Domican <jadomican@gmail.com>
2018-04-11 03:30:21 +0100

    Android Radio Button example

--- AndroidTemperatureConverter/app/src/main/java/com/jadom/androidtemperatureconverter/MainActivity.java
+++ AndroidTemperatureConverter/app/src/main/java/com/jadom/androidtemperatureconverter/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,16 +20,31 @@ public class MainActivity extends AppCompatActivity {
         EditText celsiusET = findViewById(R.id.celsiusEditText);
         String celsius = celsiusET.getText().toString().trim();
 
+        RadioGroup radioGroup = findViewById(R.id.radio_group);
+
+//        RadioButton radioC = findViewById(R.id.radioC);
+//        RadioButton radioF = findViewById(R.id.radioF);
+
+        radioGroup.getCheckedRadioButtonId();
+
         EditText fahrenheitET = findViewById(R.id.fahrenheitEditText);
         String fahrenheit = fahrenheitET.getText().toString().trim();
 
+        if (radioGroup.getCheckedRadioButtonId() == R.id.radioC) {
+            celsiusET.setText(((Double.parseDouble(fahrenheit) - 32) * (5.0 / 9.0)) + "");
+
+        }
+
+        if (radioGroup.getCheckedRadioButtonId() == R.id.radioF) {
+            fahrenheitET.setText(((Double.parseDouble(celsius) * (9.0 / 5.0)) + 32) + "");
+        }
+
+
         if (celsius.length() != 0)                // convert to fahrenheit
         {
-            fahrenheitET.setText(((Double.parseDouble(celsius) * (9.0 / 5.0)) + 32) + "");
         } else {
             if (fahrenheit.length() != 0)        // convert to celsius
             {
-                celsiusET.setText(((Double.parseDouble(fahrenheit) - 32) * (5.0 / 9.0)) + "");
             }
         }
 
